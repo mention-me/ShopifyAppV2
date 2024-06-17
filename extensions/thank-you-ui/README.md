634984ec41fce281104b39b7c5e9b202c920c632
Edward Hartwell Goose <ed@mention-me.com>
2024-06-17 22:08:09 +0100

    First pass at ThankYou pages

--- extensions/thank-you-ui/README.md
+++ extensions/thank-you-ui/README.md
@@ -1,6 +1,6 @@
 # Thank You Extension
 
-This extension is used to render the Referrer journey on the Thank You / Order Confirmation page of the Shopify Checkout.
+This extension is used to render the Referrer journey on the Thank You page of the Shopify Checkout.
 
 It is used to promote the referral program and allow a customer to share with their friends.
 
--- extensions/thank-you-ui/src/ThankYou.tsx
+++ extensions/thank-you-ui/src/ThankYou.tsx
@@ -1,114 +1,150 @@
 import {
-    BlockLayout,
-    Button,
-    Image,
-    Link,
-    View,
-    useApi,
-    useLanguage,
-    useLocalizationCountry,
-    useSettings,
-    reactExtension,
+	Banner,
+	BlockLayout,
+	Button,
+	Image,
+	Link,
+	reactExtension,
+	useApi,
+	useBillingAddress,
+	useEmail,
+	useExtensionEditor,
+	useLanguage,
+	useLocalizationCountry,
+	useSettings,
+	useSubscription,
+	useTotalAmount,
+	View,
 } from "@shopify/ui-extensions-react/checkout";
 
-import { useEffect, useState } from "react";
 
-const thankYouRender = reactExtension(
-	"purchase.thank-you.block.render",
-	() => <Extension />
-);
-export thankYouRender;
+import { useEffect, useMemo, useState } from "react";
+import { EntryPointForReferrerType, EntryPointOfferAndLink } from "@api/mention-me/src/types";
+import { isValidEnvironment, parseShopifyId } from "../../../shared/utils";
+import { fetchReferrerEntryPoint } from "../../../shared/referrerEntryPoint";
 
-// See: https://github.com/Shopify/cli/issues/3984
-// const orderStatusRender = reactExtension(
-// 	"customer-account.order-status.block.render",
-// 	() => <Extension />
-// );
-// export orderStatusRender;
 
-function Extension() {
-    const { extension } = useApi();
+const Extension = () => {
+	const { orderConfirmation } = useApi("purchase.thank-you.block.render");
 
-    const [apiResponse, setApiResponse] = useState();
+	const email = useEmail();
+	const money = useTotalAmount();
+	const billingAddress = useBillingAddress();
+	const {order} = useSubscription(orderConfirmation);
+	const language = useLanguage();
+	const country = useLocalizationCountry();
+	console.log("language", language);
+	console.log("country", country);
 
-    /*
-     * This will only be set when in real deployment
-     * TODO(EHG): Figure out what to do when we don't
-     * have this - i.e. can we check "if dev?"
-     */
-    let { mmPartnerCode } = useSettings();
+	const editor = useExtensionEditor();
 
-    const language = useLanguage();
-    // const country = useLocalizationCountry();
+	const [json, setJson] = useState<EntryPointOfferAndLink | null>(null);
 
-    // console.log("language", language);
-    // console.log("country", country);
+	let { mmPartnerCode, layout, environment } = useSettings();
 
-    useEffect(() => {
-        if (!mmPartnerCode) {
-            mmPartnerCode = "mmf1c1195b";
-            // console.log("Partner code not set");
-            // return;
-        }
+	const body: EntryPointForReferrerType = useMemo(() => {
+		return {
+			customer: {
+				emailAddress: email,
+				firstname: billingAddress?.firstName,
+				surname: billingAddress?.lastName,
+			},
+			request: {
+				partnerCode: mmPartnerCode,
+				situation: "shopify-thank-you",
+				appVersion: "v0.1",
+				appName: "mention-me-shopify-app",
+				// TODO(EHG): Figure out locales
+				localeCode: "en_GB",
+			},
+			implementation: {
+				wrapContentWithBranding: true,
+				showCloseIcon: false,
+			},
+			order: {
+				orderIdentifier: parseShopifyId(order?.id),
+				currencyCode: money.currencyCode,
+				total: String(money.amount),
+				// Use the time of the request instead of explicitly setting a time.
+				dateString: "",
+			},
+		};
+	}, [email, billingAddress, mmPartnerCode, order, money]);
 
-        fetchReferrerEntryPoint(mmPartnerCode, language.isoCode.replace("-", "_"), setApiResponse);
-    }, [fetchReferrerEntryPoint, mmPartnerCode, language, setApiResponse]);
+	useEffect(() => {
+		if (!mmPartnerCode || typeof mmPartnerCode !== "string") {
+			return;
+		}
 
-    const [visible, setVisible] = useState<boolean>(false);
+		if (!isValidEnvironment(environment)) {
+			console.error("Invalid Mention Me environment", environment);
+			return;
+		}
 
-    useEffect(() => {
-        if (apiResponse && apiResponse.url && apiResponse.defaultCallToAction) {
-            setVisible(true);
-            console.log(apiResponse);
-        }
-    }, [setVisible, apiResponse]);
+		fetchReferrerEntryPoint({
+			environment,
+			body,
+			setJson,
+		});
+	}, [body, setJson]);
 
-    return (
-        visible && (
-            <BlockLayout rows={[60, "fill"]}>
-                <View blockAlignment="center">
-                    <Button external={true} to={apiResponse.url}>
-                        {apiResponse.defaultCallToAction}
-                    </Button>
-                </View>
-                <View>
-                    {apiResponse.imageUrl && (
-                        <Link external={true} to={apiResponse.url}>
-                            <Image source={apiResponse.imageUrl} />
-                        </Link>
-                    )}
-                </View>
-            </BlockLayout>
-        )
-    );
-}
+	console.log("Json", json);
 
-function fetchReferrerEntryPoint(mmPartnerCode: string, locale: string, setApiResponse: React.Dispatch) {
-    const options = {
-        method: "POST",
-        headers: {
-            accept: "application/json",
-            "content-type": "application/json",
-        },
-        body: JSON.stringify({
-            customer: {
-                emailAddress: "customer@example.com",
-            },
-            request: {
-                partnerCode: mmPartnerCode,
-                situation: "shopify-checkout",
-                appVersion: "v0.1",
-                appName: "mention-me-shopify-app",
-                localeCode: locale,
-            },
-            implementation: {
-                wrapContentWithBranding: true,
-                showCloseIcon: false,
-            },
-        }),
-    };
+	// Now we're into the rendering part
 
-    fetch("https://demo.mention-me.com/api/entry-point/v2/referrer", options)
-        .then((response) => response.json())
-        .then((json) => setApiResponse(json));
-}
+	if (!environment || typeof environment !== "string") {
+		console.error("Mention Me environment not set");
+
+		if (editor) {
+			return <Banner
+				title="Mention Me environment not set. Choose demo for testing, production for live customers."
+				status="critical" />;
+		}
+
+		return null;
+	}
+
+	if (!mmPartnerCode || typeof mmPartnerCode !== "string") {
+		console.error("Mention Me partner code not set");
+
+		if (editor) {
+			return <Banner
+				title="Mention Me partner code needs to be set to show Mention Me journey. Click the Mention Me app on the left to add it."
+				status="critical" />;
+		}
+
+		return null;
+	}
+
+	if (!json) {
+		return null;
+	}
+
+	if (layout === "banner") {
+		return <Banner title={json.description} status="info">
+			<Link external to={json.url}>
+				{json.defaultCallToAction}
+			</Link>
+		</Banner>;
+	}
+
+	return (
+		<BlockLayout rows={[60, "fill"]}>
+			<View blockAlignment="center">
+				<Button to={json.url}>
+					{json.defaultCallToAction}
+				</Button>
+			</View>
+			<View>
+				{json.imageUrl && (
+					<Link external={true} to={json.url}>
+						<Image source={json.imageUrl} />
+					</Link>
+				)}
+			</View>
+		</BlockLayout>
+	);
+};
+
+export default reactExtension("purchase.thank-you.block.render", () => <Extension />);
+
