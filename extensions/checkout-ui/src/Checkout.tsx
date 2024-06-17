05fb4348f24bff3d6a757bac4fbb651f2875c15e
Edward Hartwell Goose <ed@mention-me.com>
2024-06-17 23:00:22 +0100

    First pass at the referee journey

--- extensions/checkout-ui/src/Checkout.tsx
+++ extensions/checkout-ui/src/Checkout.tsx
@@ -1,83 +1,138 @@
 import {
-    Link,
-    useApi,
-    useLanguage,
-    useLocalizationCountry,
-    useSettings,
-    reactExtension,
+	Banner,
+	BlockLayout,
+	BlockStack,
+	Button,
+	Form,
+	Grid,
+	Link,
+	Modal,
+	reactExtension,
+	TextField,
+	useExtensionEditor,
+	useLanguage,
+	useLocalizationCountry,
+	useSettings,
+	View,
 } from "@shopify/ui-extensions-react/checkout";
 
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import { isValidEnvironment } from "../../../shared/utils";
+import { fetchRefereeEntryPoint } from "../../../shared/refereeEntryPoint";
+import { EntryPointForRefereeType, EntryPointLink } from "@api/mention-me/dist/types";
+
+const Extension = () => {
+	const [json, setJson] = useState<EntryPointLink>();
+	const [shouldProvideEmail, setShouldProvideEmail] = useState(false);
+
+	const language = useLanguage();
+	const country = useLocalizationCountry();
+
+	console.log("language", language);
+	console.log("country", country);
+
+	const editor = useExtensionEditor();
+
+	let { mmPartnerCode, layout, environment } = useSettings();
+	console.log("environment", environment);
+
+	const body: EntryPointForRefereeType = useMemo(() => {
+		return {
+			request: {
+				partnerCode: mmPartnerCode,
+				situation: "shopify-checkout",
+				appVersion: "v0.1",
+				appName: "mention-me-shopify-app",
+				// TODO(EHG): Figure out locales
+				localeCode: "en_GB",
+			},
+			implementation: {
+				wrapContentWithBranding: true,
+				showCloseIcon: false,
+			},
+		};
+	}, [mmPartnerCode]);
+
+	useEffect(() => {
+		if (!mmPartnerCode || typeof mmPartnerCode !== "string") {
+			return;
+		}
+
+		if (!isValidEnvironment(environment)) {
+			console.error("Invalid Mention Me environment", environment);
+			return;
+		}
+
+		fetchRefereeEntryPoint({
+			environment,
+			body,
+			setJson,
+		});
+	}, [fetchRefereeEntryPoint, mmPartnerCode, language, setJson]);
+
+
+	if (!environment || typeof environment !== "string") {
+		console.error("Mention Me environment not set");
+
+		if (editor) {
+			return <Banner
+				title="Mention Me environment not set. Choose demo for testing, production for live customers."
+				status="critical" />;
+		}
+
+		return null;
+	}
+
+	if (!mmPartnerCode || typeof mmPartnerCode !== "string") {
+		console.error("Mention Me partner code not set");
+
+		if (editor) {
+			return <Banner
+				title="Mention Me partner code needs to be set to show Mention Me journey. Click the Mention Me app on the left to add it."
+				status="critical" />;
+		}
+
+		return null;
+	}
+
+	if (!json) {
+		return null;
+	}
+
+	return (
+		json && (
+			<BlockStack spacing="base">
+				<View>
+					<Link overlay={<Modal title="Welcome! Let us know who sent you." padding>
+						<BlockLayout>
+							<Form
+								onSubmit={() => {
+									console.log("Submitted");
+									setShouldProvideEmail(true);
+								}}
+								disabled={false}
+							>
+								<Grid spacing="base">
+									{shouldProvideEmail && <Banner
+										title="Sorry, we can't find your friend. Try searching with their email address too."
+										status="warning" />}
+									<TextField label="Your friends name" icon={{ source: "magnify", position: "end" }}
+											   autocomplete={false} />
+									{shouldProvideEmail && (
+										<TextField label="Your friends email" type="email"
+												   icon={{ source: "magnify", position: "end" }} autocomplete={false} />
+									)}
+									<Button accessibilityRole="submit">Submit</Button>
+								</Grid>
+							</Form>
+						</BlockLayout>
+					</Modal>}>
+						{json.defaultCallToAction}
+					</Link>
+				</View>
+			</BlockStack>
+		)
+	);
+};
 
 export default reactExtension("purchase.checkout.block.render", () => <Extension />);
-
-function Extension() {
-    const { extension } = useApi();
-
-    const [apiResponse, setApiResponse] = useState();
-
-    /*
-     * This will only be set when in real deployment
-     * TODO(EHG): Figure out what to do when we don't
-     * have this - i.e. can we check "if dev?"
-     */
-    const { mmPartnerCode } = useSettings();
-
-    const language = useLanguage();
-    // const country = useLocalizationCountry();
-
-    // console.log("language", language);
-    // console.log("country", country);
-
-    useEffect(() => {
-        if (!mmPartnerCode) {
-            console.log("Partner code not set");
-            return;
-        }
-
-        fetchRefereeEntryPoint(mmPartnerCode, language.isoCode.replace("-", "_"), setApiResponse);
-    }, [fetchRefereeEntryPoint, mmPartnerCode, language, setApiResponse]);
-
-    const [visible, setVisible] = useState<boolean>(false);
-
-    useEffect(() => {
-        if (apiResponse && apiResponse.url && apiResponse.defaultCallToAction) {
-            setVisible(true);
-        }
-    }, [setVisible, apiResponse]);
-
-    return (
-        visible && (
-            <Link external={true} to={apiResponse.url}>
-                {apiResponse.defaultCallToAction}
-            </Link>
-        )
-    );
-}
-
-function fetchRefereeEntryPoint(mmPartnerCode: string, locale: string, setApiResponse: React.Dispatch) {
-    const options = {
-        method: "POST",
-        headers: {
-            accept: "application/json",
-            "content-type": "application/json",
-        },
-        body: JSON.stringify({
-            request: {
-                partnerCode: mmPartnerCode,
-                situation: "shopify-checkout",
-                appVersion: "v0.1",
-                appName: "mention-me-shopify-app",
-                localeCode: locale,
-            },
-            implementation: {
-                wrapContentWithBranding: true,
-                showCloseIcon: false,
-            },
-        }),
-    };
-
-    fetch("https://demo.mention-me.com/api/entry-point/v2/referee", options)
-        .then((response) => response.json())
-        .then((json) => setApiResponse(json));
-}
---
+++ shared/refereeEntryPoint.ts
@@ -0,0 +1,46 @@
+import { Environment } from "./utils";
+import { EntryPointForRefereeType } from "@api/mention-me/src/types";
+
+export interface RefereeEntryPointArgs {
+	environment: Environment,
+	body: EntryPointForRefereeType,
+	setJson: React.Dispatch<any>
+}
+
+export const fetchRefereeEntryPoint = async (args: RefereeEntryPointArgs) => {
+	console.log("fetchRefereeEntryPoint", args);
+
+	const { environment, body, setJson } = args;
+
+	// TODO(EHG): Should we override the behaviour to demo when in the editor?
+	// Or perhaps just remove the order data? Or set the order value to 0 when set to production?
+	let url = "demo.mention-me.com";
+	if (environment === "production") {
+		url = "mention-me.com";
+	} else if (environment === "local") {
+		url = "mentionme.dev";
+	}
+
+	try {
+		const response = await fetch(`https://${url}/api/entry-point/v2/referee`,
+			{
+				method: "POST",
+				// mode: "no-cors",
+				credentials: "include",
+				headers: { accept: "application/json", "Content-Type": "application/json" },
+				body: JSON.stringify(body),
+			},
+		);
+
+		if (!response.ok) {
+			setJson(null);
+			console.error("Error calling entrypoint:", response);
+			return;
+		}
+
+		setJson(await response.json());
+	} catch (error) {
+		console.error("Error calling entrypoint:", error);
+		setJson(null);
+	}
+};
