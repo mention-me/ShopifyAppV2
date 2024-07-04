9be866eed877c20e5f1c8bf860167daf75960049
Edward Hartwell Goose <ed@mention-me.com>
2024-07-04 22:47:41 +0100

    Add a deploy script for deploying all the custom apps in one go

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+for FILE in `find . -name "*.toml" -depth 1`; do
+	  APP_NAME=`echo $FILE | cut -d "." -f 4`
+
+		if [ "$APP_NAME" == "custom-example" ]; then
+			echo "Skipping $APP_NAME"
+			continue
+		fi
+
+		echo "Deploying $APP_NAME"
+
+		yarn run deploy -c $APP_NAME --force
+done
