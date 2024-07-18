947578c3191a38140582874a5a811aedfe0b8543
Edward Hartwell Goose <ed@mention-me.com>
2024-07-18 17:37:50 +0100

    Make the app version automatically configurable

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+VERSION=`git rev-parse --short HEAD`
 
+# Update the version in the shared constants file
+FILE="shared/constants.ts"
+
+# Use sed to replace the version
+sed -i.bak "s/export const APP_VERSION = \".*\";/export const APP_VERSION = \"$VERSION\";/" "$FILE"
+
+# Remove the backup file created by sed
+rm "$FILE.bak"
 
 for FILE in `find . -name "*.toml" -depth 1`; do
 	  APP_NAME=`echo $FILE | cut -d "." -f 4`
--- shared/constants.ts
+++ shared/constants.ts
@@ -1,2 +1,2 @@
-export const APP_VERSION = "v0.4";
+export const APP_VERSION = "a31b692";
 export const APP_NAME = "mention-me-shopify-app";
