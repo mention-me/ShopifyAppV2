4ab0055fd72fb4c8bc720549581dc88a56ead657
Edward Hartwell Goose <ed@mention-me.com>
2024-07-19 15:07:25 +0100

    Refactor extension to make it image first, then content

--- extensions/order-status-ui/src/Extension.tsx
+++ extensions/order-status-ui/src/Extension.tsx
@@ -3,12 +3,19 @@ import {
 	BlockStack,
 	Button,
 	Heading,
+	Icon,
 	Image,
 	InlineLayout,
-	Link, SkeletonImage,
-	SkeletonText, SkeletonTextBlock,
+	InlineStack,
+	Link,
+	Popover,
+	Pressable,
+	SkeletonImage,
+	SkeletonText,
+	SkeletonTextBlock,
 	TextBlock,
 	useExtensionEditor,
+	useTranslate,
 	View,
 } from "@shopify/ui-extensions-react/checkout";
 import { useContext } from "react";
@@ -16,6 +23,8 @@ import { ReferrerJourneyContext } from "./context/ReferrerJourneyContext";
 import useReferrerEntryPoint from "./hooks/useReferrerEntryPoint";
 
 const Extension = () => {
+	const translate = useTranslate();
+
 	const {
 		partnerCode,
 		environment,
@@ -62,63 +71,87 @@ const Extension = () => {
 	}
 
 	return (
-		<BlockStack border="base"
-					borderRadius="large"
-		>
-			<InlineLayout
-				padding={["base", "base", "none", "base"]}
+		<View background="base">
+			<BlockStack border="base"
+						borderRadius="large"
 			>
-				<BlockStack padding="base"
-							spacing="base">
-					<Heading level={2}>
-						{referrerEntryPointResponse.headline}
-					</Heading>
-					<TextBlock>
-						{referrerEntryPointResponse.description}
-					</TextBlock>
-					<View blockAlignment="center"
-						  minBlockSize="fill">
-						{/* Button can't support "external". See https://github.com/Shopify/ui-extensions/issues/1835#issuecomment-2113067449
-							 And because Link can't be full width, the button is restricted in size :( */}
-						<Link external
-							  to={referrerEntryPointResponse.url}
-						>
-							<Button inlineAlignment="center">
-								{referrerEntryPointResponse.defaultCallToAction}
-							</Button>
-						</Link>
-					</View>
-				</BlockStack>
-				{referrerEntryPointResponse.imageUrl && (
+				{false && referrerEntryPointResponse.imageUrl && (
 					<View
-						// 	maxInlineSize={Style.default(200)
+
+						// maxInlineSize={Style.default(200)
 						// 	.when({ viewportInlineSize: { min: "small" } }, 200)
 						// 	.when({ viewportInlineSize: { min: "medium" } }, 200)
 						// 	.when({ viewportInlineSize: { min: "large" } }, 200)}
 					>
 						<Link external
 							  to={referrerEntryPointResponse.url}>
-							<Image borderRadius="large"
+							<Image borderRadius={["large", "large", "none", "none"]}
 								   fit="cover"
 								   source={referrerEntryPointResponse.imageUrl} />
 						</Link>
 					</View>
 				)}
-			</InlineLayout>
-			<View background="subdued"
-				  borderRadius={["none", "none", "large", "large"]}
-				  padding="base"
-			>
-				<TextBlock appearance="subdued">
-					{referrerEntryPointResponse.privacyNotice}
-					{" "}
-					<Link external
-						  to={referrerEntryPointResponse.privacyNoticeUrl}>
-						{referrerEntryPointResponse.privacyNoticeLinkText || "More info and your privacy rights"}
-					</Link>
-				</TextBlock>
-			</View>
-		</BlockStack>
+				<View borderRadius="large">
+					<BlockStack padding="loose"
+								spacing="base">
+						<Heading level={2}>
+							{referrerEntryPointResponse.headline}
+						</Heading>
+						<TextBlock>
+							{referrerEntryPointResponse.description}
+						</TextBlock>
+						<Pressable overlay={
+							<Popover>
+								<View
+									maxInlineSize={400}
+									padding="base"
+								>
+									<TextBlock appearance="subdued">
+										{referrerEntryPointResponse.privacyNotice}
+										{" "}
+										<Link external
+											  to={referrerEntryPointResponse.privacyNoticeUrl}>
+											{referrerEntryPointResponse.privacyNoticeLinkText || "More info and your privacy rights"}
+										</Link>
+									</TextBlock>
+								</View>
+							</Popover>
+						}>
+							<View background="subdued"
+								  borderRadius="large"
+								  padding="tight"
+							>
+								<InlineStack padding="extraTight"
+											 spacing="extraTight"
+								>
+									<TextBlock appearance="subdued">
+										{translate("managed-by")}
+									</TextBlock>
+
+									<Icon source="question" />
+								</InlineStack>
+							</View>
+						</Pressable>
+
+						<View blockAlignment="center"
+							  minBlockSize="fill">
+							{/*
+							Button can't support "external".
+							See https://github.com/Shopify/ui-extensions/issues/1835#issuecomment-2113067449
+						 	And because Link can't be full width, the button is restricted in size :(
+						 	*/}
+							<Link external
+								  to={referrerEntryPointResponse.url}
+							>
+								<Button inlineAlignment="center">
+									{referrerEntryPointResponse.defaultCallToAction}
+								</Button>
+							</Link>
+						</View>
+					</BlockStack>
+				</View>
+			</BlockStack>
+		</View>
 	);
 };
 
