cdb78cbc593a6f4106f361a05464c1491185f998
Ed Hartwell Goose <ed@mention-me.com>
2024-08-08 11:22:21 +0100

    Add debounce to the fetch config option

--- shared/hooks/useMentionMeShopifyConfig.ts
+++ shared/hooks/useMentionMeShopifyConfig.ts
@@ -1,6 +1,8 @@
 import { Environment, getDomainForEnvironment } from "../utils";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { APP_VERSION } from "../constants";
+import debounce from "../debounce";
+import { logError } from "../sentry";
 
 export interface Image {
 	url: string;
@@ -26,10 +28,19 @@ interface Props {
 	language: string;
 	country?: string;
 	marketId?: string;
-	marketHandle?: string
+	marketHandle?: string;
 }
 
-export const useMentionMeShopifyConfig = ({myshopifyDomain, extension, extensionLanguage, language, currency, country, marketId, marketHandle}: Props) => {
+export const useMentionMeShopifyConfig = ({
+											  myshopifyDomain,
+											  extension,
+											  extensionLanguage,
+											  language,
+											  currency,
+											  country,
+											  marketId,
+											  marketHandle,
+										  }: Props) => {
 
 	const [mentionMeConfig, setMentionMeConfig] = useState<MentionMeShopifyConfig>({
 		shopId: undefined,
@@ -42,43 +53,56 @@ export const useMentionMeShopifyConfig = ({myshopifyDomain, extension, extension
 
 	const url = getDomainForEnvironment("production");
 
-	useEffect(() => {
-		const fetchMentionMeConfig = async () => {
-			const u = new URL(`https://${url}/shopify/app/config/${myshopifyDomain}`);
-			u.searchParams.append("extensionLanguage", extensionLanguage);
-			u.searchParams.append("language", language);
-			u.searchParams.append("currency", currency);
-			u.searchParams.append("country", country);
-			u.searchParams.append("market", marketId);
-			u.searchParams.append("marketHandle", marketHandle);
-			u.searchParams.append("extension", extension);
-			u.searchParams.append("version", APP_VERSION);
+	const fetchMentionMeConfig = useCallback(async () => {
+		const u = new URL(`https://${url}/shopify/app/config/${myshopifyDomain}`);
+		u.searchParams.append("extensionLanguage", extensionLanguage);
+		u.searchParams.append("language", language);
+		u.searchParams.append("currency", currency);
+		u.searchParams.append("country", country);
+		u.searchParams.append("market", marketId);
+		u.searchParams.append("marketHandle", marketHandle);
+		u.searchParams.append("extension", extension);
+		u.searchParams.append("version", APP_VERSION);
 
-			try {
-				const response = await fetch(u.toString(),
-					{
-						method: "GET",
-						// mode: "no-cors",
-						headers: { accept: "application/json" },
-					},
-				);
+		try {
+			const response = await fetch(u.toString(),
+				{
+					method: "GET",
+					// mode: "no-cors",
+					headers: { accept: "application/json" },
+				},
+			);
 
-				if (!response.ok) {
-					throw new Error("Could not retrieve config for shop");
-				}
-
-				const json = await response.json() as MentionMeShopifyConfig;
-
-				setMentionMeConfig(json);
-				setLoading(false);
-			} catch (error) {
-				throw new Error("Caught exception trying to retrieve config for shop");
+			if (!response.ok) {
+				throw new Error("Could not retrieve config for shop");
 			}
-		};
 
-		fetchMentionMeConfig();
+			const json = await response.json() as MentionMeShopifyConfig;
+
+			setMentionMeConfig(json);
+			setLoading(false);
+		} catch (error) {
+			throw new Error("Caught exception trying to retrieve config for shop");
+		}
 	}, [country, currency, extension, extensionLanguage, language, marketHandle, marketId, myshopifyDomain, url]);
 
+	/*
+	 * Shopify is full of quirks and oddities. There are cases where certain APIs will return values asynchronously, meaning they start undefined and then later resolve to a value.
+	 * Their documentation doesn't explain this, and bugs we've raised suggests this isn't the intended behaviour.
+	 *
+	 * So, we've added a debounce - this gives the API a small amount of time (50 milliseconds at time of writing) to sort itself out. If the values change, we'll cancel the API call.
+	 * If the values don't change then we'll call the API.
+	 */
+	const debouncedFetchMentionMeConfig = debounce(fetchMentionMeConfig, 50);
+
+	useEffect(() => {
+		debouncedFetchMentionMeConfig();
+
+		return () => {
+			logError("useMentionMeShopifyConfig", "Cancelling debounce", new Error("Cancelling debounce in useMentionMeShopifyConfig"));
+			debouncedFetchMentionMeConfig.cancel();
+		};
+	}, [debouncedFetchMentionMeConfig]);
 
 	return {
 		loading,
