609c6649bc5923894136eba983e046ad7afcad55
Ed Hartwell Goose <ed@mention-me.com>
2024-08-08 11:19:35 +0100

    Add a debounce function to try and handle the Shopify oddities

--- extensions/checkout-ui/src/hooks/useRefereeSearchContent.ts
+++ extensions/checkout-ui/src/hooks/useRefereeSearchContent.ts
@@ -2,12 +2,13 @@ import { SITUATION } from "../Checkout";
 import { APP_NAME, APP_VERSION, SHOPIFY_PREVIEW_MODE_FLAG } from "../../../../shared/constants";
 import { getDomainForEnvironment, isValidEnvironment } from "../../../../shared/utils";
 import { RefereeContent } from "@api/consumer-api/src/types";
-import { useContext, useEffect } from "react";
+import { useCallback, useContext, useEffect, useMemo } from "react";
 import { RefereeJourneyContext } from "../context/RefereeJourneyContext";
-import { useExtensionEditor, useShop } from "@shopify/ui-extensions-react/checkout";
+import { useExtensionEditor, useLanguage, useShop } from "@shopify/ui-extensions-react/checkout";
 import useLocale from "../../../../shared/hooks/useLocale";
-import { useLanguage } from "@shopify/ui-extensions-react/checkout";
 import { consoleError } from "../../../../shared/logging";
+import debounce from "../../../../shared/debounce";
+import { logError } from "../../../../shared/sentry";
 
 /**
  * Function to call the Mention Me Referee EntryPoint API.
@@ -33,76 +34,91 @@ export const useRefereeSearchContent = () => {
 
 	const { isoCode } = useLanguage();
 
-	const locale = useLocale({shopifyLanguage: isoCode, defaultLocale});
+	const locale = useLocale({ shopifyLanguage: isoCode, defaultLocale });
 
-	useEffect(() => {
-		const fetchRefereeJourneyContent = async () => {
-			setLoadingRefereeContentApi(true);
+	const fetchRefereeJourneyContent = useCallback(async () => {
+		setLoadingRefereeContentApi(true);
 
-			if (!partnerCode || typeof partnerCode !== "string") {
-				return;
-			}
+		if (!partnerCode || typeof partnerCode !== "string") {
+			return;
+		}
 
-			if (!isValidEnvironment(environment)) {
-				return;
-			}
+		if (!isValidEnvironment(environment)) {
+			return;
+		}
 
-			if (!locale) {
-				return;
-			}
+		if (!locale) {
+			return;
+		}
 
-			const url = getDomainForEnvironment(environment);
+		const url = getDomainForEnvironment(environment);
 
-			const params = new URLSearchParams({
-				"request[partnerCode]": partnerCode,
-				"request[situation]": SITUATION,
-				"request[appName]": APP_NAME  + (editor ? `/${SHOPIFY_PREVIEW_MODE_FLAG}` : ""),
-				"request[appVersion]": `${myshopifyDomain}/${APP_VERSION}`,
-				"request[localeCode]": locale,
-			});
+		const params = new URLSearchParams({
+			"request[partnerCode]": partnerCode,
+			"request[situation]": SITUATION,
+			"request[appName]": APP_NAME + (editor ? `/${SHOPIFY_PREVIEW_MODE_FLAG}` : ""),
+			"request[appVersion]": `${myshopifyDomain}/${APP_VERSION}`,
+			"request[localeCode]": locale,
+		});
 
-			try {
-				const response = await fetch(`https://${url}/api/consumer/v2/referrer/search/content?${params.toString()}`,
-					{
-						method: "GET",
-						credentials: "include",
-						headers: { accept: "application/json", "Content-Type": "application/json" },
-					},
-				);
+		try {
+			const response = await fetch(`https://${url}/api/consumer/v2/referrer/search/content?${params.toString()}`,
+				{
+					method: "GET",
+					credentials: "include",
+					headers: { accept: "application/json", "Content-Type": "application/json" },
+				},
+			);
 
-				if (!response.ok) {
-					consoleError("RefereeSearchContext", "Error calling referrer search content API:", response);
+			if (!response.ok) {
+				consoleError("RefereeSearchContext", "Error calling referrer search content API:", response);
 
-					try {
-						const json = (await response.json()) as RefereeContent;
+				try {
+					const json = (await response.json()) as RefereeContent;
 
-						if (json.errors) {
-							consoleError("RefereeSearchContent", "Errors returned from Mention Me API:", json.errors.map((error) => error.message).join(", "));
-						}
-					} catch {
-						// Do nothing
+					if (json.errors) {
+						consoleError("RefereeSearchContent", "Errors returned from Mention Me API:", json.errors.map((error) => error.message).join(", "));
 					}
-
-					setErrorState(response.statusText);
-					setLoadingRefereeContentApi(false);
-
-					return;
+				} catch {
+					// Do nothing
 				}
 
-				const json = (await response.json()) as RefereeContent;
-
-				setRefereeContentApiResponse(json);
+				setErrorState(response.statusText);
 				setLoadingRefereeContentApi(false);
-			} catch (error) {
-				consoleError("RefereeSearchContent", "Error calling referee search content API:", error);
 
-				setErrorState(error?.message);
-				setLoadingRefereeContentApi(false);
+				return;
 			}
-		};
 
-		if (partnerCode && environment && locale) {
-			fetchRefereeJourneyContent();
+			const json = (await response.json()) as RefereeContent;
+
+			setRefereeContentApiResponse(json);
+			setLoadingRefereeContentApi(false);
+		} catch (error) {
+			consoleError("RefereeSearchContent", "Error calling referee search content API:", error);
+
+			setErrorState(error?.message);
+			setLoadingRefereeContentApi(false);
 		}
 	}, [partnerCode, environment, setLoadingRefereeContentApi, myshopifyDomain, setRefereeContentApiResponse, locale, setErrorState, editor]);
+
+	/*
+	 * Shopify is full of quirks and oddities. There are cases where certain APIs will return values asynchronously, meanning they start undefined and then later resolve to a value.
+	 * Their documentation doesn't explain this, and bugs we've raised suggests this isn't the intended behaviour.
+	 *
+	 * So, we've added a debounce - this gives the API a small amount of time (50 milliseconds at time of writing) to sort itself out. If the values change, we'll cancel the API call.
+	 * If the values don't change then we'll call the API.
+	 */
+	const debounceFetchRefereeJourneyContent = useMemo(() => debounce(fetchRefereeJourneyContent, 50), [fetchRefereeJourneyContent]);
+
+	useEffect(() => {
+		if (partnerCode && environment && locale) {
+			debounceFetchRefereeJourneyContent();
+		}
+
+		return () => {
+			logError("RefereeSearchContent", "Cancelling debounce", new Error("Cancelling debounce in RefereeSearchContent"));
+			debounceFetchRefereeJourneyContent.cancel();
+		};
+	}, [debounceFetchRefereeJourneyContent, environment, locale, partnerCode]);
+
 };
---
+++ shared/debounce.ts
@@ -0,0 +1,73 @@
+interface DebounceOptions {
+	shouldRunImmediately?: boolean;
+}
+
+/**
+ * Options for the debounce function
+ */
+const defaultOptions: DebounceOptions = {
+	// Immediately invoke the provided function then timeout (essentially, single throttle).
+	shouldRunImmediately: false,
+};
+
+type Cancellable<T> = T & { cancel: () => void };
+
+/**
+ * Returns a function, that, as long as it continues to be invoked, will not be triggered.
+ * The function will be called after it stops being called for N milliseconds.
+ * If `shouldRunImmediately` is passed, trigger the function on the leading edge, instead of the trailing.
+ *
+ * @param {Function} callback Function to invoke
+ * @param {Number} duration Milliseconds to wait before/after invoking the function
+ * @param {object} options A series of options for the debounce function (see above)
+ *
+ * @returns {Function} A debounce version of the function provided function
+ */
+const debounce = <T extends (...args: any) => void>(
+	callback: T,
+	duration: number,
+	options: DebounceOptions = {}
+): Cancellable<T> => {
+	const newOptions = {
+		...defaultOptions,
+		...options,
+	};
+
+	let timeout: ReturnType<typeof setTimeout> | null;
+
+	const ret = ((...args: any) => {
+		const later = () => {
+			timeout = null;
+
+			if (!newOptions.shouldRunImmediately) {
+				callback(...args);
+			}
+		};
+
+		const shouldCallNow = newOptions.shouldRunImmediately && !timeout;
+
+		if (timeout !== null) {
+			console.log("Clearing timeout", timeout);
+			clearTimeout(timeout);
+		}
+
+		console.log("Setting timeout");
+		timeout = setTimeout(later, duration);
+		console.log("Timeout set", timeout);
+
+		if (shouldCallNow) {
+			console.log("Calling now")
+			callback(...args);
+		}
+	}) as Cancellable<T>;
+
+	ret.cancel = () => {
+		if (timeout) {
+			clearTimeout(timeout);
+		}
+	}
+
+	return ret;
+};
+
+export default debounce;
