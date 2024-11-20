82630cf312715827242a1eb7eceade94f2d222f9
Ed Hartwell Goose <ed@mention-me.com>
2024-11-20 18:13:31 +0000

    Remove a ton of debug code

--- extensions/order-status-ui/src/OrderStatus.tsx
+++ extensions/order-status-ui/src/OrderStatus.tsx
@@ -1,121 +1,82 @@
 import {
-	reactExtension,
-	useCurrency,
-	useEmail,
-	useExtensionLanguage,
-	useLanguage,
-	useLocalizationCountry,
-	useLocalizationMarket,
-	useOrder,
-	usePurchasingCompany,
-	useShop,
+    reactExtension,
+    useCurrency,
+    useExtensionLanguage,
+    useLanguage,
+    useLocalizationCountry,
+    useLocalizationMarket,
+    useOrder,
+    usePurchasingCompany,
+    useShop,
 } from "@shopify/ui-extensions-react/checkout";
 import Extension from "./Extension";
 import { ReferrerJourneyProvider } from "./context/ReferrerJourneyContext";
-import { logError, setupSentry } from "../../../shared/sentry";
+import { setupSentry } from "../../../shared/sentry";
 import { useMentionMeShopifyConfig } from "../../../shared/hooks/useMentionMeShopifyConfig";
 import { consoleError } from "../../../shared/logging";
-import { useEffect, useState } from "react";
 import { ErrorBoundary } from "@sentry/react";
 
-
 const OrderStatus = () => {
-	const { myshopifyDomain } = useShop();
+    const { myshopifyDomain } = useShop();
 
-	// Setup sentry as soon as possible so that we can catch failures.
-	setupSentry(myshopifyDomain, "order-status");
+    // Setup sentry as soon as possible so that we can catch failures.
+    setupSentry(myshopifyDomain, "order-status");
 
-	const order = useOrder();
-	const currency = useCurrency();
-	const extensionLanguage = useExtensionLanguage();
-	const language = useLanguage();
-	const country = useLocalizationCountry();
-	const market = useLocalizationMarket();
+    const order = useOrder();
+    const currency = useCurrency();
+    const extensionLanguage = useExtensionLanguage();
+    const language = useLanguage();
+    const country = useLocalizationCountry();
+    const market = useLocalizationMarket();
 
-	// Debug code for https://github.com/Shopify/ui-extensions/issues/2280#issuecomment-2311068495
-	const shop = useShop();
-	const email = useEmail();
+    // As per the B2B Checkout UI guide, we can identify B2B purchases by the presence of a purchasing company.
+    // In this case, we want to turn off Mention Me features.
+    // https://shopify.dev/docs/apps/build/b2b/create-checkout-ui
+    const purchasingCompany = usePurchasingCompany();
 
-	// As per the B2B Checkout UI guide, we can identify B2B purchases by the presence of a purchasing company.
-	// In this case, we want to turn off Mention Me features.
-	// https://shopify.dev/docs/apps/build/b2b/create-checkout-ui
-	const purchasingCompany = usePurchasingCompany();
+    const { loading, mentionMeConfig } = useMentionMeShopifyConfig({
+        myshopifyDomain,
+        extension: "order-status",
+        extensionLanguage: extensionLanguage.isoCode,
+        language: language.isoCode,
+        country: country?.isoCode,
+        currency: currency.isoCode,
+        marketId: market?.id,
+        marketHandle: market?.handle,
+    });
 
-	const { loading, mentionMeConfig } = useMentionMeShopifyConfig({
-			myshopifyDomain,
-			extension: "order-status",
-			extensionLanguage: extensionLanguage.isoCode,
-			language: language.isoCode,
-			country: country?.isoCode,
-			currency: currency.isoCode,
-			marketId: market?.id,
-			marketHandle: market?.handle,
-		},
-	);
+    if (purchasingCompany) {
+        consoleError(
+            "OrderStatus",
+            "Purchasing company found. We're in a B2B situation. Mention Me features will be disabled.",
+            purchasingCompany
+        );
 
-	const [emailError, setEmailError] = useState(!!email);
+        return null;
+    }
 
-	// Debug code for https://github.com/Shopify/ui-extensions/issues/2068#issuecomment-2473982669
-	useEffect(() => {
-		if (!email) {
-			const msg = `No email received. The Shop data is: ${JSON.stringify(shop)}`;
-			logError("OrderStatus", msg, new Error(msg));
+    if (loading) {
+        return <Extension.Skeleton />;
+    }
 
-			return;
-		}
-	}, [email, shop]);
+    if (!mentionMeConfig) {
+        return null;
+    }
 
-	useEffect(() => {
-		if (!emailError) {
-			return;
-		}
+    return (
+        <ReferrerJourneyProvider mentionMeConfig={mentionMeConfig} orderId={order.id}>
+            <ErrorBoundary
+                beforeCapture={(scope, error) => {
+                    consoleError("OrderStatus", "Error boundary caught error", error);
 
-		if (email) {
-			const msg = "Email found from Shopify API after previously failing.";
-			logError("OrderStatus", msg, new Error(msg));
-
-			return;
-		}
-	}, [email, emailError]);
-
-	if (!email) {
-		setEmailError(true);
-
-		const msg = `useEmail hook did not receive an email address`;
-		logError("OrderStatus", msg, new Error(msg));
-
-		// We have no email, do not show an experience.
-		return null;
-	}
-
-	if (purchasingCompany) {
-		consoleError("OrderStatus", "Purchasing company found. We're in a B2B situation. Mention Me features will be disabled.", purchasingCompany);
-
-		return null;
-	}
-
-	if (loading) {
-		return <Extension.Skeleton />;
-	}
-
-	if (!mentionMeConfig) {
-		return null;
-	}
-
-	return <ReferrerJourneyProvider mentionMeConfig={mentionMeConfig}
-									orderId={order.id}>
-		<ErrorBoundary beforeCapture={(scope, error) => {
-			consoleError("OrderStatus", "Error boundary caught error", error);
-
-			scope.setTag("component", "OrderStatus");
-		}}>
-			<Extension extensionType="order-status" />
-		</ErrorBoundary>
-	</ReferrerJourneyProvider>;
+                    scope.setTag("component", "OrderStatus");
+                }}
+            >
+                <Extension extensionType="order-status" />
+            </ErrorBoundary>
+        </ReferrerJourneyProvider>
+    );
 };
 
 // Changing this? Don't forget to change the .toml file too.
-export default reactExtension(
-	"customer-account.order-status.block.render",
-	() => <OrderStatus />,
-);
+export default reactExtension("customer-account.order-status.block.render", () => <OrderStatus />);
--- extensions/order-status-ui/src/ThankYou.tsx
+++ extensions/order-status-ui/src/ThankYou.tsx
@@ -1,120 +1,79 @@
 import Extension from "./Extension";
 import {
-	reactExtension,
-	useApi,
-	useCurrency,
-	useEmail,
-	useExtensionLanguage,
-	useLanguage,
-	useLocalizationCountry,
-	useLocalizationMarket,
-	usePurchasingCompany,
-	useShop,
-	useSubscription,
+    reactExtension,
+    useApi,
+    useCurrency,
+    useExtensionLanguage,
+    useLanguage,
+    useLocalizationCountry,
+    useLocalizationMarket,
+    usePurchasingCompany,
+    useShop,
+    useSubscription,
 } from "@shopify/ui-extensions-react/checkout";
 import { ReferrerJourneyProvider } from "./context/ReferrerJourneyContext";
-import { logError, setupSentry } from "../../../shared/sentry";
+import { setupSentry } from "../../../shared/sentry";
 import { useMentionMeShopifyConfig } from "../../../shared/hooks/useMentionMeShopifyConfig";
 import { consoleError } from "../../../shared/logging";
 import { ErrorBoundary } from "@sentry/react";
-import { useEffect, useState } from "react";
 
 const ThankYou = () => {
-	const { myshopifyDomain } = useShop();
+    const { myshopifyDomain } = useShop();
 
-	// Setup sentry as soon as possible so that we can catch failures.
-	setupSentry(myshopifyDomain, "thank-you");
+    // Setup sentry as soon as possible so that we can catch failures.
+    setupSentry(myshopifyDomain, "thank-you");
 
-	const { orderConfirmation } = useApi("purchase.thank-you.block.render");
-	const { order } = useSubscription(orderConfirmation);
+    const { orderConfirmation } = useApi("purchase.thank-you.block.render");
+    const { order } = useSubscription(orderConfirmation);
 
-	const currency = useCurrency();
-	const extensionLanguage = useExtensionLanguage();
-	const language = useLanguage();
-	const country = useLocalizationCountry();
-	const market = useLocalizationMarket();
+    const currency = useCurrency();
+    const extensionLanguage = useExtensionLanguage();
+    const language = useLanguage();
+    const country = useLocalizationCountry();
+    const market = useLocalizationMarket();
 
-	// Debug code for https://github.com/Shopify/ui-extensions/issues/2068#issuecomment-2473982669
-	const email = useEmail();
-	const shop = useShop();
-	const [emailError, setEmailError] = useState(!!email);
+    // As per the B2B Checkout UI guide, we can identify B2B purchases by the presence of a purchasing company.
+    // In this case, we want to turn off Mention Me features.
+    // https://shopify.dev/docs/apps/build/b2b/create-checkout-ui
+    const purchasingCompany = usePurchasingCompany();
 
-	// As per the B2B Checkout UI guide, we can identify B2B purchases by the presence of a purchasing company.
-	// In this case, we want to turn off Mention Me features.
-	// https://shopify.dev/docs/apps/build/b2b/create-checkout-ui
-	const purchasingCompany = usePurchasingCompany();
+    const { loading, mentionMeConfig } = useMentionMeShopifyConfig({
+        myshopifyDomain,
+        extension: "order-status",
+        extensionLanguage: extensionLanguage.isoCode,
+        language: language.isoCode,
+        country: country?.isoCode,
+        currency: currency.isoCode,
+        marketId: market?.id,
+        marketHandle: market?.handle,
+    });
 
-	const { loading, mentionMeConfig } = useMentionMeShopifyConfig({
-			myshopifyDomain,
-			extension: "order-status",
-			extensionLanguage: extensionLanguage.isoCode,
-			language: language.isoCode,
-			country: country?.isoCode,
-			currency: currency.isoCode,
-			marketId: market?.id,
-			marketHandle: market?.handle,
-		},
-	);
+    if (purchasingCompany) {
+        return null;
+    }
 
-	useEffect(() => {
-		if (!email) {
-			const msg = `No email received. The Shop data is: ${JSON.stringify(shop)}`;
-			logError("OrderStatus", msg, new Error(msg));
+    if (loading) {
+        return <Extension.Skeleton />;
+    }
 
-			return;
-		}
-	}, [email, shop]);
+    if (!mentionMeConfig) {
+        return null;
+    }
 
-	useEffect(() => {
-		if (!emailError) {
-			return;
-		}
+    return (
+        <ReferrerJourneyProvider mentionMeConfig={mentionMeConfig} orderId={order.id}>
+            <ErrorBoundary
+                beforeCapture={(scope, error) => {
+                    consoleError("ThankYou", "Error boundary caught error", error);
 
-		if (email) {
-			const msg = "Email found from Shopify API after previously failing.";
-			logError("OrderStatus", msg, new Error(msg));
-
-			return;
-		}
-	}, [email, emailError]);
-
-	if (purchasingCompany) {
-		return null;
-	}
-
-	if (loading) {
-		return <Extension.Skeleton />;
-	}
-
-	if (!mentionMeConfig) {
-		return null;
-	}
-
-	// Debug code for https://github.com/Shopify/ui-extensions/issues/2068#issuecomment-2473982669
-	if (!email) {
-		setEmailError(true);
-
-		const msg = `useEmail hook did not receive an email address`;
-		logError("OrderStatus", msg, new Error(msg));
-
-		// We have no email, do not show an experience.
-		return null;
-	}
-
-	return <ReferrerJourneyProvider mentionMeConfig={mentionMeConfig}
-									orderId={order.id}>
-		<ErrorBoundary beforeCapture={(scope, error) => {
-			consoleError("ThankYou", "Error boundary caught error", error);
-
-			scope.setTag("component", "ThankYou");
-		}}>
-			<Extension extensionType="thank-you" />
-		</ErrorBoundary>
-	</ReferrerJourneyProvider>;
+                    scope.setTag("component", "ThankYou");
+                }}
+            >
+                <Extension extensionType="thank-you" />
+            </ErrorBoundary>
+        </ReferrerJourneyProvider>
+    );
 };
 
 // Changing this? Don't forget to change the .toml file too.
-export default reactExtension(
-	"purchase.thank-you.block.render",
-	() => <ThankYou />,
-);
+export default reactExtension("purchase.thank-you.block.render", () => <ThankYou />);
